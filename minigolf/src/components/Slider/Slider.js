cbeae3e7ccd2298ef3c0c81f8c81e68415458e3d
Micha Humbel <micha.humbel@swisscom.com>
2018-08-28 09:42:30 +0200

    - fixed store

--- minigolf/src/components/Slider/Slider.js
+++ minigolf/src/components/Slider/Slider.js
@@ -1,99 +1,102 @@
 /* eslint-disable */
 import Flickity from 'vue-flickity';
-import { mapMutations,mapGetters } from 'vuex';
-  export default {
-    components:{
-      Flickity
-    },
-    data() {
-      return {
-        flickityOptions: {
-          "pageDots": true,
-          "resize": false,
-          "prevNextButtons": false,
-          "wrapAround": true,
-          "draggable": false,
-          "initialIndex": 0
-        },
-        whichTrack: 1,
-        lastPlayer: false,
-        currentHits: 0,
-        disableDecrease: false,
-        showHitInput: false
-
-      }
-    },
-
-
-    methods:{
-      ...mapMutations(['increaseUserHits', 'setUserActive','decreaseUserHits']),
-      
-      nextTrack() {
-        this.currentHits = 0;
-        this.$refs.flickity.next();
-        this.whichTrack = this.whichTrack + 1 ;
-           //Get Selected Slide
-          //  console.log(this.$refs.flickity.selectCell(4));
-          //Select Slide
-          //  this.$refs.flickity.selectedIndex()
-          //Next slide
-          //  this.$refs.flickity.next();
+import { mapMutations, mapGetters } from 'vuex';
+export default {
+  components: {
+    Flickity
+  },
+  data() {
+    return {
+      flickityOptions: {
+        pageDots: true,
+        resize: false,
+        prevNextButtons: false,
+        wrapAround: true,
+        draggable: false,
+        initialIndex: 0
       },
+      whichTrack: 1,
+      lastPlayer: false,
+      currentHits: 0,
+      disableDecrease: false,
+      showHitInput: false,
+      activeUser: {}
+    };
+  },
 
-      nextPlayer(){
-        if(this.lastPlayer){
-          this.nextTrack();
-        }else{
+  methods: {
+    ...mapMutations(['increaseUserHits', 'setUserActive', 'decreaseUserHits']),
+
+    nextTrack() {
+      this.currentHits = 0;
+      this.$refs.flickity.next();
+      this.whichTrack = this.whichTrack + 1;
+      //Get Selected Slide
+      //  console.log(this.$refs.flickity.selectCell(4));
+      //Select Slide
+      //  this.$refs.flickity.selectedIndex()
+      //Next slide
+      //  this.$refs.flickity.next();
+    },
+
+    nextPlayer() {
+      if (this.lastPlayer) {
+        this.nextTrack();
+      } else {
         this.currentHits = 0;
         this.updateActiveUser();
         this.showHitInput = true;
-        }
-      },
-      //Increase Hits per Track
-      addHit () {
-        console.log(this.activeUser.id);
-        this.increaseUserHits({userId: this.activeUser.id, trackId: this.whichTrack});
-        this.currentHits +=1;
-        console.log(this.activeUser);
+      }
+    },
+    //Increase Hits per Track
+    addHit() {
+      if (this.activeUser.track[this.whichTrack - 1].hits !== 7) {
+        console.log('in slider active user: ', this.activeUser);
+        this.increaseUserHits({
+          userId: this.activeUser.id,
+          trackId: this.whichTrack
+        });
+        this.currentHits += 1;
         this.disableDecrease = false;
-        if(this.activeUser.track[this.whichTrack -1].hits  >= 7){
-          console.log("zu viel");
-          this.nextPlayer();
-        }
-      },
-      //Decrease Hits per Track
-      removeHit(){
-        if(!this.currentHits == 0){
-          this.decreaseUserHits({userId: this.activeUser.id, trackId: this.whichTrack});
-          this.currentHits -=1;
-          
-        }else{
-          this.disableDecrease = true;
-        }
-        
-      },
-      //Update the User which is currently playing
-      updateActiveUser() {
-        if (!this.lastPlayer){ this.setUserActive(this.activeUser.id + 1);
-        if (this.getUsers[this.getUsers.length - 1].active === true){
+      } else {
+        this.nextPlayer();
+      }
+    },
+    //Decrease Hits per Track
+    removeHit() {
+      if (!this.currentHits == 0) {
+        this.decreaseUserHits({
+          userId: this.activeUser.id,
+          trackId: this.whichTrack
+        });
+        this.currentHits -= 1;
+      } else {
+        this.disableDecrease = true;
+      }
+    },
+    //Update the User which is currently playing
+    updateActiveUser() {
+      if (!this.lastPlayer) {
+        this.setUserActive(this.activeUser.id + 1);
+        this.setActiveUser();
+        if (this.getUsers[this.getUsers.length - 1].active === true) {
           this.lastPlayer = true;
         }
       } else {
         this.nextTrack();
       }
-      }
-
     },
-   created(){
-     this.setUserActive(1);
-     this.showHitInput = true;
-     console.log(this.getUsers);
-    },
-    computed:{
-      ...mapGetters(['getUsers']),
-      activeUser() {
-        return this.getUsers.filter(user => user.active === true)[0];
-      }
+    setActiveUser() {
+      this.activeUser = this.getUsers.filter(user => user.active === true)[0];
     }
-      
+  },
+  created() {
+    this.setUserActive(1);
+    this.setActiveUser();
+    this.showHitInput = true;
+    console.log(this.getUsers);
+  },
+  computed: {
+    ...mapGetters(['getUsers'])
   }
+};
--- minigolf/src/store/modules/client.js
+++ minigolf/src/store/modules/client.js
@@ -5,62 +5,74 @@ const state = {
   users: []
 
   //    test:true
-}
+};
 
 // getters
 const getters = {
-
-  getUsers () {
-    return state.users
+  getUsers() {
+    return state.users;
   }
-
-}
+};
 
 // actions
 const actions = {
-
-  setUpState ({commit}, users) {
-   
-    const tracks = []
-    for (let i = 1; i <= 18; i++) {
-      tracks.push({trackId: i, hits: 0})
-    }
+  setUpState({ commit }, users) {
     for (let x = 0; x < users.length; x++) {
+      const tracks = [];
+      for (let i = 1; i <= 18; i++) {
+        tracks.push({ trackId: i, hits: 0 });
+      }
       const newUser = {
         id: x + 1,
         name: users[x].name,
         track: tracks,
         active: false
-      }
-      commit('setUser', newUser)
+      };
+      commit('setUser', newUser);
     }
   }
-}
+};
 
 // mutations
 const mutations = {
-  setUser ($state, user) {
-    $state.users.push(user)
+  setUser($state, user) {
+    $state.users.push(user);
   },
-  increaseUserHits ($state, infos) {
-    console.log(infos);
-    $state.users.filter(user => user.id === infos.userId)[0].track.filter(track => track.trackId === infos.trackId)[0].hits += 1
-  },
-  decreaseUserHits ($state, infos) {
-    $state.users.filter(user => user.id === infos.userId)[0].track.filter(track => track.trackId === infos.trackId)[0].hits -= 1
-  },
-  setUserActive ($state, userId) {
+  increaseUserHits($state, infos) {
+    console.log('state: ', $state);
+    console.log('infos: ', infos);
+    console.log(
+      'filtered user: ',
+      $state.users.filter(user => user.id === infos.userId)[0]
+    );
+    $state.users
+      .filter(user => user.id === infos.userId)[0]
+      .track.filter(track => track.trackId === infos.trackId)[0].hits += 1;
 
-    for(var i = 0; i < $state.users.length; i++){
-      $state.users[i].active = false
+    console.log(
+      'filtered user track hits: ',
+      $state.users
+        .filter(user => user.id === infos.userId)[0]
+        .track.filter(track => track.trackId === infos.trackId)[0]
+    );
+    console.log('users in state: ', $state.users);
+  },
+  decreaseUserHits($state, infos) {
+    $state.users
+      .filter(user => user.id === infos.userId)[0]
+      .track.filter(track => track.trackId === infos.trackId)[0].hits -= 1;
+  },
+  setUserActive($state, userId) {
+    for (var i = 0; i < $state.users.length; i++) {
+      $state.users[i].active = false;
     }
-    $state.users.filter(user => user.id === userId)[0].active = true
+    $state.users.filter(user => user.id === userId)[0].active = true;
   }
-}
+};
 
 export default {
   state,
   getters,
   actions,
   mutations
-}
+};
