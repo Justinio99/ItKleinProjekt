f6bfb29eb6219f117fdc75e02c4c48d4cdf3ac85
taajuni2 <33315361+taajuni2@users.noreply.github.com>
2018-11-13 15:12:25 +0100

    video geit zur helfti

--- minigolf/src/components/Slider/Slider.vue
+++ minigolf/src/components/Slider/Slider.vue
@@ -8,6 +8,7 @@
   <div class="carousel-cell"></div>
   <div class="carousel-cell"></div>
   <div class="carousel-cell"></div>
+
 </Flickity>
 </div>
 <div class="counter-container">
@@ -36,3 +37,5 @@
 
 <style src="./Slider.scss" lang="scss" scoped></style>
 <script src="./Slider.js"></script>
+<!--<video autoplay class="spot" height="400" width="350" poster="@/assets/mini.jpg" loop controls><source
+  src="https://www.youtube.com/watch?v=D4X8CZYiURA" type="video/webm"></video>!-->
--- minigolf/src/components/Start/Start.vue
+++ minigolf/src/components/Start/Start.vue
@@ -2,12 +2,17 @@
 <div class="container">
 
      <div class="players">
-    <input style="width: 181px" class="addPlayer" type="text" :placeholder="`Spieler ${player.id}`" v-for="(player, index) in players" :key="player.id" v-model="players[index].name">
+          <input style="width: 181px" class="addPlayer"  type="text" :placeholder="`Spieler ${player.id}`" v-for="(player, index) in players" :key="player.id" v-model="players[index].name">
     </div>
     <button @click="addPlayer" class="btn-addPlayer" style="font-size: 12px">
         Spieler hinzufügen
     </button>
    <button data-hover="Lets Go !" @click="startGame"><div>Start</div></button>
+
+
+  
+
+
 </div>
 </template>
 
