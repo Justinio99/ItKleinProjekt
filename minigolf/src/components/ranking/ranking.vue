c7bfff41c21edede4da15e0f655074c4783d0d04
Spring Justin <Justin.Spring@swisscom.com>
2018-11-27 15:47:48 +0100

    work

--- minigolf/src/components/ranking/ranking.vue
+++ minigolf/src/components/ranking/ranking.vue
@@ -1,8 +1,15 @@
 <template>
 <div class="container">
-
-Hallo
 <button @click="resetUser">Close</button>
+<button @click="askForPlayer = true" v-show="isLoggedIn">Abspeichern</button>
+<div class="whichUser" v-if="askForPlayer">
+    <p>Welcher Spieler warst du?</p>
+     <select v-model="selectedValue">
+         <option disabled value="">Please select one</option>
+         <option v-for="user in users" :key="user" :value="user.id" >{{user.name}}</option>
+     </select>
+     <button @click="saveToDatabase">Save</button>
+</div>
 </div>
 </template>
 
