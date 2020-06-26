ec01fa0ff68d57e09bf24a6e8b8ed763c781e2f6
Justin <justin@gmail.com>
2020-06-26 17:23:46 +0200

    wip

--- minigolf/src/components/SignUp/signup.js
+++ minigolf/src/components/SignUp/signup.js
@@ -1,35 +1,34 @@
-import firebase, { firestore } from 'firebase'
+import firebase from 'firebase'
 
 export default {
-      name: 'signup',
-    data() {
-      return {
-        picture: 'https://www.mcbern.ch/fileadmin/logos/logo_mcbmw.png',
-        email:'',
-        password:'',
-        firstname: '',
-        lastname: '',
-        isClubMember: false
+  name: 'signup',
+  data () {
+    return {
+      picture: 'https://www.mcbern.ch/fileadmin/logos/logo_mcbmw.png',
+      email: '',
+      password: '',
+      firstname: '',
+      lastname: '',
+      isClubMember: false
 
-      }
-    },
-    methods:{
-      signUp(){
-        firebase.auth().createUserAndRetrieveDataWithEmailAndPassword(this.email, this.password).then((user) => {
+    }
+  },
+  methods: {
+    signUp () {
+      firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then((user) => {
+        const firestore = firebase.firestore()
 
-          const firestore = firebase.firestore();
-
-          firestore.collection('users').doc(user.user.uid).set({
-            firstname: this.firstname,
-            lastname: this.lastname,
-            isClubMember: this.isClubMember
-          })
-          alert('Dein Account wurde erfolgreich erstellt!');
-          this.$router.push('/login');
-        }).catch((err) =>{
-          alert(err.message);
+        firestore.collection('users').doc(user.user.uid).set({
+          firstname: this.firstname,
+          lastname: this.lastname,
+          isClubMember: this.isClubMember
         })
-      }
-    },
-
+        alert('Dein Account wurde erfolgreich erstellt!')
+        this.$router.push('/login')
+      }).catch((err) => {
+        alert(err.message)
+      })
+    }
   }
+
+}
